b0dbfa9f27d1d206dd61cf4e103507a09223cbe0
Jiajian Yang <yangjeep@gmail.com>
2012-02-05 17:57:11 +0000

    SVN r229 — partial metadata reconstruction
    Original SVN author: jyang425
    Original SVN timestamp: 2012-02-05T17:57:11.741336Z
    Recovered files introduced: 1
    Referenced files missing from recovered snapshot: 0
    
    This commit is reconstructed from SVN wc.db metadata. File contents are
    the recovered r230 working-copy bytes for files whose last-changed revision
    is r229; intermediate historical contents and diffs are unavailable.
    
    Recovered paths:
    - cumodp/include/cumodp_simple.h

---
+++ cumodp/include/cumodp_simple.h
@@ -0,0 +1,22 @@
+#ifndef _CUMODP_SIMPLE_H_
+#define _CUMODP_SIMPLE_H_
+
+#include "../include/ct_fft.h"
+#include "../include/ct_fft_mont.h"
+#include "../include/cudautils.h"
+#include "../include/cumodp.h"
+#include "../include/defines.h"
+#include "../include/fft_aux.h"
+#include "../include/inlines.h"
+#include "../include/list_stockham.h"
+#include "../include/montmulmod.h"
+#include "../include/printing.h"
+#include "../include/rdr_poly.h"
+#include "../include/scube.h"
+#include "../include/stockham.h"
+#include "../include/stockham_mont.h"
+#include "../include/subres.h"
+#include "../include/types.h"
+#include "../include/unipoly.h"
+
+#endif
